4d69efe36ce95dac99f5e2ea739a081ea739199d
Abdellah BENBOUCHTA <abbenbouchta@gmail.com>
2015-04-21 09:07:17 +0100

    Create sifac.sh

---
+++ sifac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /opt/odoo/addons
+rm -R sifa_compta
+mkdir sifa_compta
+cd sifa_compta
+git clone https://github.com/fatma-isi/sifa_compta.git --depth 1 --branch develop --single-branch .
+cd ..
+chmod -R 775 sifa_compta
+chown -R odoo:odoo sifa_compta
+service odoo-server restart
+exit 0
