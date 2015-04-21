253e938223830739a364dc79afd7c9db2c82d5ed
Abdellah BENBOUCHTA <abbenbouchta@gmail.com>
2015-04-21 09:06:28 +0100

    Create sifa.sh

---
+++ sifa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /opt/odoo/addons
+rm -R sifa
+mkdir sifa
+cd sifa
+git clone https://github.com/abdellatifkarroum/sifa.git --depth 1 --branch develop --single-branch .
+cd ..
+chmod -R 775 sifa
+chown -R odoo:odoo sifa
+service odoo-server restart
+exit 0
