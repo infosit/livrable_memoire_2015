639c087f049c0fc82079b8ba6d8032a723d12128
Abdellah BENBOUCHTA <abbenbouchta@gmail.com>
2015-04-22 16:55:29 +0100

    Update sifa.sh

--- sifa.sh
+++ sifa.sh
@@ -3,7 +3,7 @@ cd /opt/odoo/addons
 rm -R sifa
 mkdir sifa
 cd sifa
-git clone https://github.com/abdellatifkarroum/sifa.git --depth 1 --branch develop --single-branch .
+git clone https://github.com/abbenbouchta/sifa.git --depth 1 --branch develop --single-branch .
 cd ..
 chmod -R 775 sifa
 chown -R odoo:odoo sifa
