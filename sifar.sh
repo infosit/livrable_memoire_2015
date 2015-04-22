41e6a1a18bd2ea8794bb755aa560048a31bd181c
Abdellah BENBOUCHTA <abbenbouchta@gmail.com>
2015-04-22 16:56:50 +0100

    Update sifar.sh

--- sifar.sh
+++ sifar.sh
@@ -3,7 +3,7 @@ cd /opt/odoo/addons
 rm -R sifa_reports
 mkdir sifa_reports
 cd sifa_reports
-git clone https://github.com/abdellatifkarroum/sifa_reports.git --depth 1 --branch develop --single-branch .
+git clone https://github.com/abbenbouchta/sifa_reports.git --depth 1 --branch develop --single-branch .
 cd ..
 chmod -R 775 sifa_reports
 chown -R odoo:odoo sifa_reports
